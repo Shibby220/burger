bdbd65cd9990c6c10be6b00a37250a63ec3bd6bd
Daniel Spira <daniel.b.spira@gmail.com>
2019-09-21 19:08:29 -0700

    initial app functionality working as intended

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Double-Double Animal Style');
+INSERT INTO burgers (burger_name) VALUES ('Royale with Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Whopper');
+INSERT INTO burgers (burger_name) VALUES ('ShackBurger');
+INSERT INTO burgers (burger_name) VALUES ('Buttery Jack');
